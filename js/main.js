4f589245887baeafd9d7be8d2ad9cee19decefa1
SimoneScapin <simonescap87@gmail.com>
2022-03-16 15:32:42 +0100

    finito

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    // #il contenuto della proprietà "el" sarà l'id dell'elemento di partenza di Vue
+    el : '#app',
+    data: {
+        messaggio : '',
+        url : '',
+        classe:'',
+    },
+})
+
